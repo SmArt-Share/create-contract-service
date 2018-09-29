a839b3877bf48c55f15567a300b6541c62ff5dcd
GIDEON <cheesyelmo@gmail.com>
2018-09-29 12:10:24 -0500

    Basic lambda code
    this does very little right now

---
+++ lambda/index.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2018 SmArt Share
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+exports.handler = async (event, context) => {
+    console.log(event);
+    return createChain();
+
+}
+
+function createChain(){
+    var data = JSON.stringify(false);
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("POST", "https://api-2445581893456.production.gw.apicast.io/v2/chains");
+
+    xhr.send(data);
+}
